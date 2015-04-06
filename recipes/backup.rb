866f31020a95685f601705c718a7c2fb841120eb
Tomasz Setkowski <tom@ai-traders.com>
2015-04-06 11:25:00 +0000

    install cinder backup service recipe

---
+++ recipes/backup.rb
@@ -0,0 +1,21 @@
+
+class ::Chef::Recipe # rubocop:disable Documentation
+  include ::Openstack
+end
+
+include_recipe 'openstack-block-storage::cinder-common'
+
+platform_options = node['openstack']['block-storage']['platform']
+
+platform_options['cinder_backup_packages'].each do |pkg|
+  package pkg do
+    options platform_options['package_overrides']
+    action :upgrade
+  end
+end
+
+case node['openstack']['block-storage']['backup']['driver']
+when 'cinder.backup.drivers.ceph'
+  include_recipe 'ceph'
+# TODO ensure cephx authorization without repeating code already in volume recipe
+end
